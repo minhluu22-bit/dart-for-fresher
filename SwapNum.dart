e2acc81318718595ef003f9eddd278132828259f
minhluu22-bit <Minh86446@st.vimaru.edu.vn>
2023-02-13 10:38:24 +0700

    add swap function

--- SwapNum.dart
+++ SwapNum.dart
@@ -2,11 +2,21 @@
 
 import 'dart:io';
 
+void swap(int a, int b) {
+  int tmp;
+  tmp = a;
+  a = b;
+  b = tmp;
+  print("First number is: ${a}");
+  print("Second number is: ${b}");
+}
+
 void main() {
   stdout.write("Enter first number: ");
   int num1 = int.parse(stdin.readLineSync()!);
   stdout.write("Enter second number: ");
   int num2 = int.parse(stdin.readLineSync()!);
+  swap(num1, num2);
   num1 = num1 ^ num2;
   num2 = num1 ^ num2;
   num1 = num1 ^ num2;
