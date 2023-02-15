fec9ed1c04723f39afcf7ff7ddbb22998d3d1d0f
minhluu22-bit <75942030+minhluu22-bit@users.noreply.github.com>
2023-02-15 23:05:52 +0700

    p3

--- dart/Loop_Condition/MulTable.dart
+++ dart/Loop_Condition/MulTable.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 void main() {
   for (int i = 1; i <= 10; i++) {
-    for (int j = 1; j <= 10; j++) {
+    for (int j = 1; j <= 9; j++) {
       stdout.write("$j * $i = ${i * j} \t");
     }
     print("");
