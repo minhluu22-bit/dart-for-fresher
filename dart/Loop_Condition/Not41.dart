588f70ac14bcefdd627cc177a039475532d68045
minhluu22-bit <75942030+minhluu22-bit@users.noreply.github.com>
2023-02-17 23:38:35 +0700

    update

--- dart/Loop_Condition/Not41.dart
+++ dart/Loop_Condition/Not41.dart
@@ -1,9 +1,10 @@
 void main() {
   int sum = 0;
   for (int i = 1; i <= 100; i++) {
-    if (i == 41){
+    if (i == 41) {
       continue;
     }
     sum += i;
   }
+  print("The sum of 1 to 100 but not 41 is: $sum");
 }
